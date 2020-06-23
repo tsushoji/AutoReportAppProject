68c3e82bdfa7d8de443a97a2a8ad8c239f8402c8
Shota Tsuji <s.tuji3007sho@gmail.com>
2020-06-23 09:27:54 +0900

    2020/06/23 UPDATE：データcsvファイル出力ロジック変更

--- AutoReportWinApp/InputDailyReportForm.cs
+++ AutoReportWinApp/InputDailyReportForm.cs
@@ -14,10 +14,13 @@ namespace AutoReportWinApp
 {
     public partial class InputDailyReportForm : Form
     {
+        public static int controlNum = 0;
+        private static readonly string _createDataFilePath = "C:/Users/Shota Tsuji/Desktop/DailyReportData.csv";
         public InputDailyReportForm()
         {
             InitializeComponent();
         }
+        public static string CreateDataFilePath { get => _createDataFilePath; }
 
         private void InputDailyReportForm_Load(object sender, EventArgs e)
         {
@@ -34,11 +37,13 @@ namespace AutoReportWinApp
 
         private void buttonCreateData_Click(object sender, EventArgs e)
         {
-            using (var dailyReport = new DailyReport(DateTime.Parse(this.textBox1.Text), this.textBox2.Text, this.textBox3.Text, this.textBox4.Text))
-            using (var csvWriter = new CsvWriter())
+            using (var fileStream = new FileStream(InputDailyReportForm.CreateDataFilePath, FileMode.Append, FileAccess.Write))
+            using (var streamWriter = new StreamWriter(fileStream, Encoding.Default))
             {
-                string[] writingData = { DailyReport.ControlNum.ToString(), dailyReport.CreateDate.ToString(), dailyReport.ImplementationContent, dailyReport.ScheduledForNextDay, dailyReport.Task };
-                csvWriter.WriteRow(writingData);
+                controlNum++;
+                string[] writingData = { InputDailyReportForm.controlNum.ToString(), this.textBox1.Text, this.textBox2.Text, this.textBox3.Text, this.textBox4.Text };
+                var writingLine = string.Join(",", writingData);
+                streamWriter.WriteLine(writingLine);
             }
         }
     }
